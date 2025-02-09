550018dc7e993d305d5800548c3a6732481bc2d2
Iyeba Kallon <iyebalphakallon@gmail.com>
2025-02-09 20:17:51 +0000

    fixed a  bug

--- src/utilities/arrays.js
+++ src/utilities/arrays.js
@@ -1,5 +1,15 @@
 // Remove duplicates from an array
 export const deduplicateArray = (arr) => [...new Set(arr)];
 
-// Find the max number in an array
-export const findMax = (arr) => Math.max(...arr);
+// Find the intersection of two arrays
+export const arrayIntersection = (arr1, arr2) => arr1.filter(value => arr2.includes(value));
+
+// Shuffle an array
+export const shuffleArray = (arr) => {
+  let shuffled = [...arr];
+  for (let i = shuffled.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]]; // Swap elements
+  }
+  return shuffled;
+};
