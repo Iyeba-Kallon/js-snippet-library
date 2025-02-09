5e3122b1f8badbd6e6d1502e0911ed98378c380a
Iyeba Kallon <iyebalphakallon@gmail.com>
2025-02-09 19:47:51 +0000

    initial commit

---
+++ src/utilities/arrays.js
@@ -0,0 +1,5 @@
+// Remove duplicates from an array
+export const deduplicateArray = (arr) => [...new Set(arr)];
+
+// Find the max number in an array
+export const findMax = (arr) => Math.max(...arr);
---
+++ src/utilities/strings.js
@@ -0,0 +1,5 @@
+// Capitalize the first letter of a string
+export const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
+
+// Reverse a string
+export const reverseString = (str) => str.split('').reverse().join('');
